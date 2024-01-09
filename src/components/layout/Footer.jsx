2c74186eec938be297fcb90885bf90f7a5d7b151
Marcestordeur <marcstord@gmail.com>
2024-01-09 16:33:51 -0300

    Folders created, description jsx

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,3 @@
+// Must include a button or a link that leads to another page detailing the repository used, GITHUB.
+// Must include a button or a link that leads to a page to report problems, etc.
+// It must be the color palette used in the trello.
