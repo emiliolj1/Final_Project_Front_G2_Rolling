8da43f2b94246c56b27d5d5ecc5d7c3c83a1bb33
Marcestordeur <marcstord@gmail.com>
2024-01-11 19:21:37 -0300

    calling and usign the handleSubmit

--- src/components/layout/Register.jsx
+++ src/components/layout/Register.jsx
@@ -1,3 +1,28 @@
 import { useForm } from 'react-hook-form';
 import './components/styles/styleLogin.css'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+
+const RegisterComponent = () => {
+  const { register, handleSubmit, formState:{errors}} = useForm()
+  
+  // create the function handleSubmit for the fetch of the backend and show the "your account has been created"
+  const onSubmit = async() => {
+    // with full data we can copy all the properties of the data with the operator spray (...)
+    const fulldata = {...data, role:'Client'}
+    // we call https post to the fetch's url
+    const response = await fetch('https://localhost:4000/users',{
+      method:'POST',
+      headers:{'Content-type':'application/json'},
+      credentials:'include',
+      body: JSON.stringify(fulldata)
+    })
+    const responseData =  await response.json()
+
+    console.log(responseData);
+  }
+
+  return(
+    <>
+    </>
+  )
+}
