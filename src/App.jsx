2e4a995c42e3d289605810a35d9c82de20004dcd
Emilio Luna Jandar <lunaemilio269@gmail.com>
2024-01-20 15:28:49 -0300

    Change Name of html title and fix errors(delete import's in main.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './components/styles/navbar.css';
 import NavBar from './components/layout/NavBar';
-import Footer from "./components/layout/Footer";
+
 
 function App() {
   
-
   return (
     <>
       <NavBar/>
