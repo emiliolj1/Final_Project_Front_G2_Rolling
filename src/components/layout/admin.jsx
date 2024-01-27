16323d00aa761886bacf1ea35c4cffa552ec1d0d
marcelo <marcstord@gmail.com>
2024-01-27 18:46:18 -0300

    admin created

--- src/components/layout/admin.jsx
+++ src/components/layout/admin.jsx
@@ -6,28 +6,37 @@ import Table from 'react-bootstrap/Table';
 
 const admin = () => {
     const { handleSubmit, register, reset } = useForm()
-    const [createShow, setCShow] = useState(false);
-    const [delteShow, setDShow] = useState(false);
-    const [users, setUSers] = useState([]);
+    const [CreateproductShow, setCShow] = useState(false);
+    const [deleteProductShow, setDShow] = useState(false);
+    const [CreateCanchaShow, setCCShow] = useState(false);
+    const [deleteCanchaShow, setDCShow] = useState(false);
+    const [users, setUsers] = useState([]);
     const [products, setProducts] = useState([])
 
     const handleClose = () => {
         setCShow(false);
         setDShow(false);
+        setCCShow(false);
+        setDCShow(false);
         reset();
     };
 
-    const handleShow = () => setShow(true);
+    const handleShow = () => {
+        setCShow(true);
+        setDShow(true);
+        setCCShow(true);
+        setDCShow(true);
+    };
     
     const getUsers = async () => {
-        const response = await fetch ('',{
+        const response = await fetch (`https://localHost:3000/users/${id}`,{
             method:'GET',
             headers:{'Content-type':'application/json'},
             credentials:'include'
         })
     const responseData = await response.json()
     console.log(responseData);
-    setUSers(responseData.users)
+    setUsers(responseData.users)
     }
     useEffect(() => {
         getUsers();
@@ -35,7 +44,7 @@ const admin = () => {
 
     const getProducts = async () => {
         try {
-            const response = await fetch('',{
+            const response = await fetch(`https://localHost:3000/product/${id}`,{
                 method:'GET',
                 headers: { 'Content-type': 'application/json' },
                 credentials: 'include',
@@ -50,9 +59,27 @@ const admin = () => {
         getProducts();
     }, []);
 
+    const getCanchas = async () => {
+        try {
+            const response = await fetch(`https://localHost:3000/canchas/${id}`,{
+                method:'GET',
+                headers: { 'Content-type': 'application/json' },
+                credentials: 'include',
+            })
+            const responseData = await response.json()
+            setCanchas(responseData.canchas)
+        } catch (error) {
+            console.log('error en el fecht de los canchas', error);
+        }
+    }
+    useEffect(() => {
+        getCanchas();
+    }, []);
+
+
     const CreateProduct = async () => {
         try {
-            const response = await fetch('',{
+            const response = await fetch(``,{
                 method:'POST',
                 headers:{'Content-type':'application/json'},
                 credentials:'include'
@@ -60,24 +87,56 @@ const admin = () => {
             const responseData = await response.json()
             console.log(responseData)
             getProducts();
-            handleClose()            
+            handleClose();  
         } catch (error) {
             console.log('error creando el producto', error);
         }
     }
 
-
-    const Delete = async () => {
+    const CreateCancha = async () => {
         try {
-            const response = await fetch(`/${product.Id}`,{
+            const response = await fetch(``,{
+                method:'POST',
+                headers:{'Content-type':'application/json'},
+                credentials:'include'
+            })
+            const responseData = await response.json();
+            console.log(responseData);
+            getCanchas();
+            handleClose();
+        } catch (error) {
+            
+        }
+    }
+
+
+    const ProductDelete = async () => {
+        try {
+            const response = await fetch(`/${Id}`,{
                 method:'DELETE',
                 headers:{'Content-type':'application/json'},
                 credentials:'include'
             })
             const responseData = await response.json();
             console.log(responseData);
-            getProducts()
-            handleClose()
+            getProducts();
+            handleClose();
+        } catch (error) {
+            
+        }
+    }
+
+    const CanchaDelete = async () => {
+        try {
+            const response = await fetch(`${id}`,{
+                method:'DELETE',
+                headers:{'Content-type':'application/json'},
+                credentials:'include'
+            })
+            const responseData = await response.json();
+            console.log(responseData);
+            getCanchas();
+            handleClose();
         } catch (error) {
             
         }
@@ -125,15 +184,42 @@ const admin = () => {
                 ))}
             </tbody>
         </Table>
+        <Table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {canchas.map((cancha) => (
+                    <tr key={cancha.id}>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+
         <Button variant="primary" onClick={() => setCShow(true)}>
             Crear Producto
         </Button>
         <Button variant="primary" onClick={() => setDShow(true)}>
             Borrar Producto
         </Button>
+        <Button variant="primary" onClick={() => setCCShow(true)}>
+            Crear cancha
+        </Button>
+        <Button variant="primary" onClick={() => setDDShow(true)}>
+            Borrar cancha
+        </Button>
 
         <Modal 
-        show={createShow} 
+        show={CreateproductShow} 
         onHide={handleClose} 
         animation={false}>
         <Modal.Header closeButton>
@@ -195,16 +281,140 @@ const admin = () => {
         </Modal>
 
         <Modal 
-        show={delteShow} 
+        show={deleteProductShow} 
         onHide={handleClose} 
         animation={false}>
         <Modal.Header closeButton>
             <Modal.Title>Borrar Producto</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form onSubmit={handleSubmit((data) => Delete(data))}>
+                <Form onSubmit={handleSubmit((data) => ProductDelete(data))}>
+                <Form.Group className='m-2'>
+                    <Form.Label>ID</Form.Label>
+                    <Form.Control
+                    className='ms-0 me-5 pe-5' 
+                    type='text'
+                    placeholder='Enter your name...'
+                    isInvalid={!!errors.email}
+                    // the method register allows you to register an input or select element and apply validations rules
+                    // operator (...) allows an iterable to expand in places where 0+ arguments are expected. It is mostly used in the variable array where there is more than 1 value is expected. 
+                    {...register('name', {required:'this field is required'})}
+                    />
+                <Form.Control.Feedback type='invalid'>{errors.name?.message}</Form.Control.Feedback>
+                </Form.Group>
+
                 <Form.Group className='m-2'>
                     <Form.Label></Form.Label>
+                    <Form.Control
+                    className='ms-0 me-5 pe-5' 
+                    type='text'
+                    placeholder=''
+                    isInvalid={!!errors.email}
+                    // the method register allows you to register an input or select element and apply validations rules
+                    // operator (...) allows an iterable to expand in places where 0+ arguments are expected. It is mostly used in the variable array where there is more than 1 value is expected. 
+                    {...register('name', {required:'this field is required'})}
+                    />
+                <Form.Control.Feedback type='invalid'>{errors.name?.message}</Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className='m-2'>
+                    <Form.Label></Form.Label>
+                    <Form.Control
+                    className='ms-0 me-5 pe-5' 
+                    type='text'
+                    placeholder='Enter your name...'
+                    isInvalid={!!errors.email}
+                    // the method register allows you to register an input or select element and apply validations rules
+                    // operator (...) allows an iterable to expand in places where 0+ arguments are expected. It is mostly used in the variable array where there is more than 1 value is expected. 
+                    {...register('name', {required:'this field is required'})}
+                    />
+                <Form.Control.Feedback type='invalid'>{errors.name?.message}</Form.Control.Feedback>
+                </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary">
+                    Save Changes
+                </Button>
+            </Modal.Footer>
+        </Modal>
+
+        <Modal 
+        show={CreateCanchaShow} 
+        onHide={handleClose} 
+        animation={false}>
+        <Modal.Header closeButton>
+            <Modal.Title>Crear cancha</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={handleSubmit((data) => CreateCancha(data))}>
+                <Form.Group className='m-2'>
+                    <Form.Label></Form.Label>
+                    <Form.Control
+                    className='ms-0 me-5 pe-5' 
+                    type='text'
+                    placeholder='Enter your name...'
+                    isInvalid={!!errors.email}
+                    // the method register allows you to register an input or select element and apply validations rules
+                    // operator (...) allows an iterable to expand in places where 0+ arguments are expected. It is mostly used in the variable array where there is more than 1 value is expected. 
+                    {...register('name', {required:'this field is required'})}
+                    />
+                <Form.Control.Feedback type='invalid'>{errors.name?.message}</Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className='m-2'>
+                    <Form.Label></Form.Label>
+                    <Form.Control
+                    className='ms-0 me-5 pe-5' 
+                    type='text'
+                    placeholder=''
+                    isInvalid={!!errors.email}
+                    // the method register allows you to register an input or select element and apply validations rules
+                    // operator (...) allows an iterable to expand in places where 0+ arguments are expected. It is mostly used in the variable array where there is more than 1 value is expected. 
+                    {...register('name', {required:'this field is required'})}
+                    />
+                <Form.Control.Feedback type='invalid'>{errors.name?.message}</Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className='m-2'>
+                    <Form.Label></Form.Label>
+                    <Form.Control
+                    className='ms-0 me-5 pe-5' 
+                    type='text'
+                    placeholder='Enter your name...'
+                    isInvalid={!!errors.email}
+                    // the method register allows you to register an input or select element and apply validations rules
+                    // operator (...) allows an iterable to expand in places where 0+ arguments are expected. It is mostly used in the variable array where there is more than 1 value is expected. 
+                    {...register('name', {required:'this field is required'})}
+                    />
+                <Form.Control.Feedback type='invalid'>{errors.name?.message}</Form.Control.Feedback>
+                </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary">
+                    Save Changes
+                </Button>
+            </Modal.Footer>
+        </Modal>
+
+        <Modal 
+        show={deleteCanchaShow} 
+        onHide={handleClose} 
+        animation={false}>
+        <Modal.Header closeButton>
+            <Modal.Title>Borrar Cancha</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={handleSubmit((data) => CanchaDelete(data))}>
+                <Form.Group className='m-2'>
+                    <Form.Label>ID</Form.Label>
                     <Form.Control
                     className='ms-0 me-5 pe-5' 
                     type='text'
