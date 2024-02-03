62eb0e13a02a05a931c89898fb5a3c381c04190a
marcelo <marcstord@gmail.com>
2024-02-02 21:43:42 -0300

    bug fix and create change

--- src/components/layout/Login.jsx
+++ src/components/layout/Login.jsx
@@ -3,8 +3,7 @@ import { useForm } from 'react-hook-form';
 import '../styles/Login.css'
 import { Container, Row, Col, Form, Button,} from 'react-bootstrap'
 
-function Login({ setUser }) {
-
+const Login = ({ setUser }) => {
   const { register, handleSubmit, formState:{errors}} = useForm()
   // create the function handleSubmit for the fetch of the backend and show the "your account has been created"
 
@@ -13,13 +12,12 @@ function Login({ setUser }) {
     const fulldata = {...data, role:'Client'}
     // we call https post to the fetch's url
     const response = await fetch('https://localhost:4000/users',{
-      method:'GET',
+      method:'POST',
       headers:{'Content-type':'application/json'},
       credentials:'include',
       body: JSON.stringify(fulldata)
     })
     const responseData =  await response.json()
-
     console.log(responseData);
   }
 
@@ -53,10 +51,11 @@ function Login({ setUser }) {
           isInvalid={!!errors.email}
           // the method register allows you to register an input or select element and apply validations rules
           // operator (...) allows an iterable to expand in places where 0+ arguments are expected. It is mostly used in the variable array where there is more than 1 value is expected. 
-          {...register('name', {required:'this field is required'})}
+          {...register('name', {required:'este campo es requerido'}, {maxLength: 20}, {minLength: 4})}
           />
           <Form.Control.Feedback type='invalid'>{errors.name?.message}</Form.Control.Feedback>
         </Form.Group>
+        
         <Form.Group className='m-2'>
           <Form.Label>Correo</Form.Label>
           <Form.Control
@@ -66,7 +65,7 @@ function Login({ setUser }) {
           isInvalid={!!errors.email}
           // the method register allows you to register an input or select element and apply validations rules
           // operator (...) allows an iterable to expand in places where 0+ arguments are expected. It is mostly used in the variable array where there is more than 1 value is expected. 
-          {...register('email', {required:'this field is required'})}
+          {...register('email', {required:'este campo es requerido'}, {maxLength: 25}, {minLength: 5})}
           />
             <Form.Control.Feedback type='invalid'>{errors.email?.message}</Form.Control.Feedback>
           </Form.Group>
@@ -80,7 +79,7 @@ function Login({ setUser }) {
               isInvalid={!!errors.password}
               // the method register allows you to register an input or select element and apply validations rules
               // operator (...) allows an iterable to expand in places where 0+ arguments are expected. It is mostly used in the variable array where there is more than 1 value is expected. 
-              {...register('password', {required:'this field is required'})}
+              {...register('password', {required:'este campo es requerido'}, {maxLength: 25}, {minLength: 5})}
               />
             <Form.Control.Feedback type='invalid'>{errors.password?.message}</Form.Control.Feedback>
           </Form.Group>
