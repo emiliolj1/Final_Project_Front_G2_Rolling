7a6d2d40143695c30b17f3665eff38ce5b3078bf
Emilio Luna Jandar <lunaemilio269@gmail.com>
2024-02-12 22:12:53 -0300

    Changes in productDetail

--- src/components/pages/ProductDetail.jsx
+++ src/components/pages/ProductDetail.jsx
@@ -36,14 +36,14 @@ const ProductDetail = () => {
       <Container>
         {
           <Row  className='pb-5 mt-0 g-0'>
-          <Col className='border border-success bg-success col-sm-6 rounded-start'>
-          <Image src={products.Url}  className='aling-items-center justify-content-center mt-4 pt-5 ps-3 mb-4 pb-4 ' fluid/>
-          </Col>
-          <Col className='border border-success box-shadow bg-success rounded-end col-sm-6'>
-          <h2 className='text-light text-center mt-5'>{products.Title}</h2>
-          <h5 className='text-light text-center ps-3'>{products.description}</h5>
-          <h2 className='text-light text-center mt-5'>{products.Price}</h2>
-          </Col>
+            <Col className='border border-success bg-success col-sm-6 rounded-start'>
+              <Image src={products.Url}  className='aling-items-center justify-content-center mt-4 pt-5 ps-3 mb-4 pb-4 ' fluid/>
+            </Col>
+            <Col className='border border-success box-shadow bg-success rounded-end col-sm-6'>
+              <h2 className='text-light text-center mt-5'>{products.Title}</h2>
+              <h5 className='text-light text-center ps-3'>{products.description}</h5>
+              <h2 className='text-light text-center mt-5'>{products.Price}</h2>
+            </Col>
           </Row>
         }
       </Container>
